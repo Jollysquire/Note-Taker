839b41cf8a5cbce61ae8542857cf7afdfe90953f
Jolly <jmurphy106@my.bcit.ca>
2021-04-07 23:26:17 -0700

    added short comments on how to implement each feature

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// Dark/Light mode
+// - When button is pressed toggle off original all colors (or class might be easier)
+// - add new colors above oringinal to make them the default, uses cascading, 
+//  This is done in the css not js
+
+// Cancel/Notes button
+// - On button click toggle visibility of relevant elements.
+// - Could only find how to toggle visibilty by id, 
+//   may have to restructure index or find new solution
+
+//Save button
+// - Prompts users for input, which is used as key in the array.
+// - Takes current text in text area and makes it value, this should be easy
+//   using innerHTML and assigning the text to a variable.
+// - Add key to notes list again using innerHTML imagine to update entire list 
+//   with a for loop and adding each key every time, unless I can append.
+
+//Load notes
+// - Should be similar to saving with the way I check elements, but instead use
+// inner to update the textarea not save it.
+// - Not entirely sure how to use notes title as button if they are being updated.
